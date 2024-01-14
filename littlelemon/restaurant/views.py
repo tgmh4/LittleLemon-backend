921c31566fa5b005b493c22215c42c89bfe19f12
H <choongjianhan@gmail.com>
2024-01-15 04:04:00 +0800

    setting up models

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+# class UserViewSet(viewsets.ModelViewSet):
+#    queryset = User.objects.all() 
+#    serializer_class = UserSerializer
+#    permission_classes = [permissions.IsAuthenticated] 
+
